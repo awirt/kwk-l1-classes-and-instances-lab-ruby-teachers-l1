43b01882f5670ac476d5cd1f2f1aa9bc6d10f556
Andrew Wirt <awjwirt@gmail.com>
2018-07-13 00:27:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+
+#variable types 
+  #local variable (example a = 3) 
+  #instance variable (example @a = something)
+  
+  
+class Dog 
+end 
+
+#creating new instances and storing them as a local variable 
+fido = 
+snoopy = 
+lassie = 
+  
